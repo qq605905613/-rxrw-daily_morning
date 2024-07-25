9fb0d35d43e588e3ea2406b9e18c21efbbb92b9c
qq605905613 <605905613@qq.com>
2024-07-25 15:08:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+
+data = {"city":{"value":"上海"}
 res = wm.send_template(user_id, template_id, data)
 print(res)
